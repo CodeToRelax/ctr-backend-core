d5f7b4c3ebf8088750bc6caf95b602574b737a8b
Bofa Alsarah <bofaalsarah@gmail.com>
2020-07-15 17:35:22 +0200

    refactor(encryption/process): accept key as a binary instead of ssm param

--- ctr_core/encryption/process.py
+++ ctr_core/encryption/process.py
@@ -1,28 +1,18 @@
 from cryptography.fernet import Fernet
-from ctr_core.aws.aws_ssm import fetch_ssm_config
 
 
-def load_key(key_name):
-    """
-    Load the generated key
-    """
-    return fetch_ssm_config(key_name).decode()
-
-
-def encrypt_message(message, key_name):
+def encrypt_message(message, key):
     """
     Encrypts a message
     """
-    key = load_key(key_name)
     encoded_message = message.encode()
     f = Fernet(key)
     return f.encrypt(encoded_message)
 
 
-def decrypt_message(encrypted_message, key_name):
+def decrypt_message(encrypted_message, key):
     """
     Decrypts an encrypted message
     """
-    key = load_key(key_name)
     f = Fernet(key)
     return f.decrypt(encrypted_message)
